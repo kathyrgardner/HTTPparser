0871c8d25fd78ed0cfbd48f3554cc1e7f34b6a9a
Kathy Gardner <kathyrgardner@gmail.com>
2019-09-17 18:42:35 -0400

    add parsing header

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ module.exports = function parseHttp(request) {
     const parsedMessage = {
 
     }
-    const parts = request.split(' ')
-    parsedMessage.verb = parts[0]
+    const lines = request.split('\n')
+    
+    let parts = lines[0].split(' ')
+    parsedMessage.verb = parts[0].toUpperCase()
     parsedMessage.path = parts[1]
     parsedMessage.version = parts[2]
+
+    if (lines[1] === undefined) {
+        return parsedMessage
+    }
+    parsedMessage.headers = {}
+    parts = lines[1].split(': ')
+    
+    const key = parts[0]
+    const value = parts[1]
+    parsedMessage.headers[key] = value
     return parsedMessage
+
 }
--- spec/parserSpec.js
+++ spec/parserSpec.js
@@ -13,4 +13,31 @@ describe('parser http', function(){
         }
         expect(output).toEqual(expected)
     })
+    it ('normalizes the verb', function(){
+        // arrange
+        const request = 'get / HTTP/1.1'
+        // act
+        const output = parseHttp(request)
+        // assert
+        const expected = {
+            verb: 'GET',
+            path: '/',
+            version: 'HTTP/1.1'
+        }
+        expect(output).toEqual(expected)
+    })
+    it ('sets content-type headers', function(){
+        const request = 'GET / HTTP/1.1\ncontent-type: text/HTML; charset=utf8'
+        const output = parseHttp(request)
+        const expected ={
+            verb: 'GET',
+            path: '/',
+            version: 'HTTP/1.1',
+            headers: {
+                'content-type': 'text/HTML; charset=utf8'
+            }
+        }
+        expect(output).toEqual(expected)
+
+    })
 })
